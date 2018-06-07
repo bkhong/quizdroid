03d8f985541ff321b968e713f453b242b17db000
Brandon Hong <brandonhong@D-10-19-16-31.dhcp4.washington.edu>
2018-06-06 23:09:34 -0700

    part5

--- app/src/main/java/edu/washington/bkhong/quizdroid/MainActivity.kt
+++ app/src/main/java/edu/washington/bkhong/quizdroid/MainActivity.kt
@@ -1,10 +1,15 @@
 package edu.washington.bkhong.quizdroid
 
+import android.app.AlertDialog
+import android.app.Dialog
 import android.content.Context
+import android.content.DialogInterface
 import android.content.Intent
 import android.support.v7.app.AppCompatActivity
 import android.os.Bundle
 import android.os.Environment
+import android.provider.Settings
+import android.support.v4.app.ActivityCompat
 import android.util.Log
 import android.view.*
 import android.widget.BaseAdapter
@@ -16,6 +21,10 @@ import java.io.File
 import java.io.FileInputStream
 import java.lang.reflect.Array
 import java.util.jar.Manifest
+import android.net.NetworkInfo
+import android.net.ConnectivityManager
+
+
 
 class MainActivity : AppCompatActivity() {
     private lateinit var listView: ListView
@@ -25,13 +34,55 @@ class MainActivity : AppCompatActivity() {
         setContentView(R.layout.activity_main)
         setSupportActionBar(findViewById(R.id.my_toolbar))
 
-        listView = findViewById(R.id.listView)
-        listView.adapter = customAdapter(this)
-        listView.setOnItemClickListener { parent, view, position, id ->
-            val intent = Intent(this, Quiz::class.java)
-            intent.putExtra("subject", position)
-            startActivity(intent)
+        if (!OnlineRepository().topics.isEmpty()) { //if no questions file is present don't do anything
+            listView = findViewById(R.id.listView)
+            listView.adapter = customAdapter(this)
+            listView.setOnItemClickListener { parent, view, position, id ->
+                val intent = Intent(this, Quiz::class.java)
+                intent.putExtra("subject", position)
+                startActivity(intent)
+            }
         }
+
+        if (!isConnected(this)) {
+            if (isAirplaneModeOn(this)) {
+                val builder = AlertDialog.Builder(this)
+                val inflater = this.layoutInflater
+                val dialogView = inflater.inflate(R.layout.custom_dialog, null)
+                builder.setTitle("Do you want to turn off airplane mode?")
+                builder.setPositiveButton("Yes", DialogInterface.OnClickListener { dialog, which ->
+                    startActivity(Intent(android.provider.Settings.ACTION_SETTINGS))
+                })
+                builder.setNegativeButton("No", DialogInterface.OnClickListener { dialog, which ->
+
+                })
+                builder.create().show()
+            } else {
+                val builder = AlertDialog.Builder(this)
+                val inflater = this.layoutInflater
+                val dialogView = inflater.inflate(R.layout.custom_dialog, null)
+                builder.setView(dialogView)
+                builder.setTitle("Device is not currently connected")
+                builder.setNeutralButton("OK", DialogInterface.OnClickListener { dialog, which ->
+
+                })
+                builder.create().show()
+            }
+        }
+    }
+
+    // Returns whether or not airplane mode is on
+    private fun isAirplaneModeOn(context: Context): Boolean {
+        return Settings.System.getInt(context.contentResolver,
+                Settings.Global.AIRPLANE_MODE_ON, 0) != 0
+    }
+
+    // Returns whether or not the phone is connected to internet
+    fun isConnected(context: Context): Boolean {
+        val cm = context
+                .getSystemService(Context.CONNECTIVITY_SERVICE) as ConnectivityManager
+        val activeNetwork = cm.activeNetworkInfo
+        return activeNetwork != null && activeNetwork.isConnectedOrConnecting
     }
 
     override fun onCreateOptionsMenu(menu: Menu?): Boolean {
@@ -45,7 +96,6 @@ class MainActivity : AppCompatActivity() {
     }
 
     private class customAdapter(context: Context): BaseAdapter() {
-
         private val myContext: Context
 
         init {
--- app/src/main/java/edu/washington/bkhong/quizdroid/Preferences.kt
+++ app/src/main/java/edu/washington/bkhong/quizdroid/Preferences.kt
@@ -1,10 +1,24 @@
 package edu.washington.bkhong.quizdroid
 
+import android.Manifest
+import android.app.AlarmManager
+import android.app.DownloadManager
+import android.app.PendingIntent
+import android.content.BroadcastReceiver
+import android.content.Context
+import android.content.Intent
+import android.content.pm.PackageManager
+import android.net.Uri
 import android.support.v7.app.AppCompatActivity
 import android.os.Bundle
+import android.os.Environment
+import android.support.v4.app.ActivityCompat
+import android.support.v4.content.ContextCompat
+import android.util.Log
 import android.widget.Button
 import android.widget.NumberPicker
 import android.widget.TextView
+import android.widget.Toast
 
 class Preferences : AppCompatActivity() {
 
@@ -17,14 +31,27 @@ class Preferences : AppCompatActivity() {
         val urlView = findViewById<TextView>(R.id.urlInput)
         val urlSubmitBtn = findViewById<Button>(R.id.urlSubmitButton)
         val numberPicker = findViewById<NumberPicker>(R.id.numberPicker)
+        val alarmManager = getSystemService(Context.ALARM_SERVICE) as AlarmManager
+        val intent = Intent(this, CustomReceiver::class.java)
+        numberPicker.minValue = 1
         numberPicker.maxValue = 10
-        numberPicker.minValue = 0
         numberPicker.wrapSelectorWheel = false
 
-
         urlSubmitBtn.setOnClickListener {
-            //Implement later
+            if (ContextCompat.checkSelfPermission(this, Manifest.permission.WRITE_EXTERNAL_STORAGE) !=
+                    PackageManager.PERMISSION_GRANTED) {
+                ActivityCompat.requestPermissions(this,
+                        arrayOf(Manifest.permission.WRITE_EXTERNAL_STORAGE), 0)
+            } else {
+                intent.putExtra("url", urlView.text.toString())
+                val pendingIntent = PendingIntent.getBroadcast(this, 1, intent, PendingIntent.FLAG_UPDATE_CURRENT)
+                val minutes = numberPicker.value * 60 * 1000
+                alarmManager.setRepeating(AlarmManager.RTC_WAKEUP, System.currentTimeMillis(), minutes.toLong(), pendingIntent)
+                Toast.makeText(this, "Attempting download at ${urlView.text}",
+                        Toast.LENGTH_SHORT).show()
+            }
         }
+
         numberPicker.setOnValueChangedListener { picker, oldVal, newVal ->
             //Implement later
         }
